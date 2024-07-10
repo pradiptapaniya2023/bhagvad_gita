65e9a03ed7288ab8f8de5608348203849706481b
Pradip Tapaniya <pradiptapaniya2023.dev@gmail.com>
2024-07-10 14:53:17 +0530

    Widget add

--- lib/screens/1_home_screen.dart
+++ lib/screens/1_home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:coin_king/widget/1_topcoins.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    PageController pgController = PageController();
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -93,21 +98,70 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 15 , left: 20 , right: 20),
+            padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
             child: Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(border: Border.all(color: Colors.white)),
-              child: PageView(
-                children: [
-                ],
-              ),
+                height: 230,
+                width: double.infinity,
+                // decoration:
+                //     BoxDecoration(border: Border.all(color: Colors.white)),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: PageView.builder(
+                        itemCount: 2,
+                        controller: pgController,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Appwidgets().topCoinsCard("BTC", "Coin",
+                                      "1st", "Bitcoin", "Active"),
+                                  Appwidgets().topCoinsCard("ETH", "Coin",
+                                      "2nd", "Ethereum", "Active"),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Appwidgets().topCoinsCard("USDT", "Token",
+                                      "3nd", "Tether", "Active"),
+                                  Appwidgets().topCoinsCard("BNB", "Coin",
+                                      "4nd", "Binannce Coin", "Active"),
+                                ],
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                    SmoothPageIndicator(
+                      controller: pgController,
+                      count: 2,
+                      effect: WormEffect(
+                          dotHeight: 8,
+                          dotWidth: 8,
+                          spacing: 8,
+                          dotColor: Colors.white,
+                          activeDotColor: Colors.yellow),
+                    )
+                  ],
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10, left: 20),
+            child: Text(
+              "Coins",
+              style: TextStyle(
+                  fontFamily: "fontfamily", color: Colors.yellow, fontSize: 17),
             ),
-          )
+          ),
+
         ],
       ),
     );
   }
 }
-
-
--- lib/widget/1_topcoins.dart
+++ lib/widget/1_topcoins.dart
@@ -1,13 +1,110 @@
 import 'package:flutter/material.dart';
 
-Widget topCoinsCard() {
-  return Container(
-    height: 50,
-    width: 90,
-    child: Card(
-      color: Colors.white,
-      elevation: 5,
-      shadowColor: Colors.white,
-    ),
-  );
+class Appwidgets {
+  Widget topCoinsCard(
+    String top1,
+    String top2,
+    String top3, String center1, String bottom1,
+  ) {
+    return Container(
+      height: 102,
+      width: 160,
+      child: Card(
+        color: Colors.white,
+        elevation: 9,
+        shadowColor: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Container(
+                    height: 26,
+                    width: 48,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Center(
+                      child: Text(
+                        top1,
+                        style: TextStyle(
+                            fontFamily: "regfont", color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Container(
+                    height: 26,
+                    width: 48,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Center(
+                      child: Text(
+                        top2,
+                        style: TextStyle(
+                            fontFamily: "regfont", color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Container(
+                    height: 26,
+                    width: 48,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Center(
+                      child: Text(
+                        top3,
+                        style: TextStyle(
+                            fontFamily: "regfont", color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 5 , left: 5),
+                  child: Text(
+                    center1,
+                    style: TextStyle(
+                        fontFamily: "regfont", fontWeight: FontWeight.bold , fontSize: 18),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 4 , left: 5),
+              child: Container(
+                height: 26,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.green[900],
+                    borderRadius: BorderRadius.circular(8)),
+                child: Center(
+                  child: Text(
+                    bottom1,
+                    style: TextStyle(
+                        fontFamily: "regfont", color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
 }
